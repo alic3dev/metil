7769e7d07fa9f0456293b18e2644546eca3d25c8
alic3 <alic3dev@gmail.com>
2025-09-17 12:04:27 -0700

    `rendering_properties`:add->{`color_clear`};

--- sources/metil_rendering/metil_renderer.m
+++ sources/metil_rendering/metil_renderer.m
@@ -146,10 +146,10 @@ metil_renderer_on_initialize_function metil_renderer_on_initialize = (void*)0;
 
   MTLRenderPassDescriptor* descriptor_render_pass = metal_kit_view.currentRenderPassDescriptor;
   descriptor_render_pass.colorAttachments[0].clearColor = MTLClearColorMake(
-    0.0f,
-    0.0f,
-    0.0f,
-    1.0f
+    self->rendering_properties.color_clear.x,
+    self->rendering_properties.color_clear.y,
+    self->rendering_properties.color_clear.z,
+    self->rendering_properties.color_clear.w
   );
 
   encoder_render = [command_buffer renderCommandEncoderWithDescriptor: descriptor_render_pass];
