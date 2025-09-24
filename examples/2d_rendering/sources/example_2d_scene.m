5d2a03ae5ece594551fd087f006dcae62cac2cde
alic3 <alic3dev@gmail.com>
2025-09-23 19:48:34 -0700

    `example_2d_rendering`:update_example

--- examples/2d_rendering/sources/example_2d_scene.m
+++ examples/2d_rendering/sources/example_2d_scene.m
@@ -14,6 +14,39 @@
 
 #include <stdio.h>
 
+const unsigned int unbuffered_indices[
+  example_2d_scene_length_unbuffered_indices
+] = {
+  0, 1, 2,
+  0, 3, 2
+};
+
+const struct clic3_vector4_float unbuffered_vertices[
+  example_2d_scene_length_unbuffered_vertices
+] = {
+  {
+    .x = 0.0f,
+    .y = 0.0f,
+    .z = 0.0f,
+    .w = 1.0f
+  }, {
+    .x = 0.2f,
+    .y = 0.0f,
+    .z = 0.0f,
+    .w = 1.0f
+  }, {
+    .x = 0.2f,
+    .y = 0.2f,
+    .z = 0.0f,
+    .w = 1.0f
+  }, {
+    .x = 0.0f,
+    .y = 0.2f,
+    .z = 0.0f,
+    .w = 1.0f
+  }
+};
+
 void example_2d_scene_initialize(
   struct metil_scene* scene,
   id<MTLDevice> metal_kit_device
@@ -31,191 +64,105 @@ void example_2d_scene_initialize(
   scene->poll = example_2d_scene_poll;
   scene->destroy = example_2d_scene_destroy;
 
-  scene->length_objects = 1;
+  scene->length_objects = 100; 
   scene->objects = realloc(
     scene->objects,
     sizeof(struct metil_object*) *
     scene->length_objects
   );
 
-  scene->objects[
-    0
-  ] = malloc(
-    sizeof(struct metil_object)
-  );
-
-  metil_object_initialize(
-    scene->objects[
-      0
-    ]
-  );
-
-  metil_mesh_initialize(
-    &scene->objects[
-      0
-    ]->mesh
-  );
-
-  scene->objects[
-    0
-  ]->mesh.length_indices = 6;
-
-  scene->objects[
-    0
-  ]->mesh.length_vertices = 4;
-
-  scene->objects[
-    0
-  ]->mesh.indices = realloc(
-    scene->objects[
-      0
-    ]->mesh.indices,
-    sizeof(unsigned int) *
-    scene->objects[
-      0
-    ]->mesh.length_indices
-  );
-
-  scene->objects[
-    0
-  ]->mesh.vertices = realloc(
-    scene->objects[
-      0
-    ]->mesh.vertices,
-    sizeof(struct clic3_vector4_float) *
-    scene->objects[
-      0
-    ]->mesh.length_vertices
-  );
-
-  scene->objects[
-    0
-  ]->mesh.vertices[0].x = -0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[0].y = -0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[0].z = 0.0f;
-  scene->objects[
-    0
-  ]->mesh.vertices[0].w = 1.0f;
-
-  scene->objects[
-    0
-  ]->mesh.vertices[1].x = 0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[1].y = -0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[1].z = 0.0f;
-  scene->objects[
-    0
-  ]->mesh.vertices[1].w = 1.0f;
-
-  scene->objects[
-    0
-  ]->mesh.vertices[2].x = 0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[2].y = 0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[2].z = 0.0f;
-  scene->objects[
-    0
-  ]->mesh.vertices[2].w = 1.0f;
-
-  scene->objects[
-    0
-  ]->mesh.vertices[3].x = -0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[3].y = 0.1f;
-  scene->objects[
-    0
-  ]->mesh.vertices[3].z = 0.0f;
-  scene->objects[
-    0
-  ]->mesh.vertices[3].w = 1.0f;
-
-  scene->objects[
-    0
-  ]->mesh.indices[0] = 0;
-
-  scene->objects[
-    0
-  ]->mesh.indices[1] = 1;
-
-  scene->objects[
-    0
-  ]->mesh.indices[2] = 2;
-
-  scene->objects[
-    0
-  ]->mesh.indices[3] = 0;
-
-  scene->objects[
-    0
-  ]->mesh.indices[4] = 3;
-
-  scene->objects[
-    0
-  ]->mesh.indices[5] = 2;
-
-  scene->objects[
-    0
-  ]->vertices = [metal_kit_device
-    newBufferWithBytes: scene->objects[
-      0
-    ]->mesh.vertices
-    length: scene->objects[
-      0
-    ]->mesh.length_vertices * sizeof(
+  id<MTLBuffer> vertices = [metal_kit_device
+    newBufferWithBytes: unbuffered_vertices
+    length: example_2d_scene_length_unbuffered_vertices * sizeof(
       struct clic3_vector4_float
     )
     options: MTLResourceStorageModeShared
   ];
 
-  scene->objects[
-    0
-  ]->indices = [metal_kit_device
-    newBufferWithBytes: scene->objects[
-      0
-    ]->mesh.indices
-    length: scene->objects[
-      0
-    ]->mesh.length_indices * sizeof(
+  id<MTLBuffer> indices = [metal_kit_device
+    newBufferWithBytes: unbuffered_indices
+    length: example_2d_scene_length_unbuffered_indices * sizeof(
       unsigned int
     )
     options: MTLResourceStorageModeShared
   ];
 
-  scene->objects[0]->mesh.positioning = metil_mesh_positioning_static;
+  for (
+    unsigned char index_object = 0;
+    index_object < scene->length_objects;
+    ++index_object
+  ) {
+    scene->objects[
+      index_object
+    ] = malloc(
+      sizeof(struct metil_object)
+    );
 
-  scene->objects[
-    0
-  ]->data = [metal_kit_device
-    newBufferWithLength: sizeof(metil_kit_data_frame_object)
-    options: MTLResourceStorageModeShared
-  ];
+    metil_object_initialize(
+      scene->objects[
+        index_object
+      ]
+    );
 
-  metil_kit_data_frame_object* data_object = scene->objects[
-    0
-  ]->data.contents;
+    metil_mesh_initialize(
+      &scene->objects[
+        index_object
+      ]->mesh
+    );
 
-  data_object->id = 0;
-  data_object->color.x = 0.25f;
-  data_object->color.y = 0.1f;
-  data_object->color.z = 1.0f;
-  data_object->color.w = 1.0f;
+    scene->objects[
+      index_object
+    ]->vertices = vertices;
 
-  scene->objects[
-    0
-  ]->position.z = (
-    1.0f
-  );
+    scene->objects[
+      index_object
+    ]->indices = indices;
+
+    scene->objects[index_object]->mesh.length_indices = (
+      example_2d_scene_length_unbuffered_indices
+    );
+
+    scene->objects[index_object]->mesh.length_vertices = (
+      example_2d_scene_length_unbuffered_vertices
+    );
+
+    scene->objects[index_object]->mesh.positioning = metil_mesh_positioning_static;
+
+    scene->objects[
+      index_object
+    ]->data = [metal_kit_device
+      newBufferWithLength: sizeof(metil_kit_data_frame_object)
+      options: MTLResourceStorageModeShared
+    ];
+
+    metil_kit_data_frame_object* data_object = scene->objects[
+      index_object
+    ]->data.contents;
+
+    data_object->id = index_object;
+    data_object->color.x = (float) index_object / (scene->length_objects - 1);
+    data_object->color.y = (float) ((index_object + 33) % scene->length_objects) / (scene->length_objects - 1);
+    data_object->color.z = (float) ((index_object + 66) % scene->length_objects) / (scene->length_objects - 1);
+    data_object->color.w = 1.0f;
+
+    scene->objects[
+      index_object
+    ]->position.x = (
+      (float) (index_object % 10)
+    ) * 0.2f - 1.0f;
+
+    scene->objects[
+      index_object
+    ]->position.y = (
+      (float) (index_object / 10)
+    ) * 0.2f - 1.0f;
+
+    scene->objects[
+      index_object
+    ]->position.z = (
+      1.0f
+    );
+  }
 }
 
 void example_2d_scene_poll(
@@ -223,64 +170,60 @@ void example_2d_scene_poll(
 ) {
   metil_scene_poll_default(scene);
 
-  struct metil_rendering_properties* rendering_properties = (
-    (struct metil_rendering_properties*) scene->data
-  );
+  float brightness_minimum = 0.25f;
 
-  rendering_properties->color_clear.x = fmod(
-    rendering_properties->camera.ratio_aspect * (float) ((scene->time / 50) % 3000),
-    100.0f
-  ) / 100.0f;
+  for (
+    unsigned char index_object = 0;
+    index_object < scene->length_objects;
+    ++index_object
+  ) {
+    metil_kit_data_frame_object* data_object = scene->objects[
+      index_object
+    ]->data.contents;
 
-  rendering_properties->color_clear.y = fmod(
-    rendering_properties->camera.ratio_aspect * (float) ((scene->time / 42) % 4000),
-    100.0f
-  ) / 100.0f;
+    struct clic3_vector3_unsigned_int time_offset = {
+      .x = scene->time + (index_object + 2) * 20,
+      .y = scene->time + (index_object + 3) * 30,
+      .z = scene->time + (index_object + 1) * 10
+    };
 
-  rendering_properties->color_clear.z = fmod(
-    rendering_properties->camera.ratio_aspect  * (float) ((scene->time / 33) % 5000),
-    100.0f
-  ) / 100.0f;
-
-  struct clic3_vector4_float* a = scene->objects[
-    0
-  ]->vertices.contents;
-
-  float time = (float) (scene->time % 4000);
-
-  if (time >= 2000.0f) {
-    time = 4000.0f - time;
+    data_object->color.x = (
+      ((time_offset.x / 1000) % 2 == 0
+        ? (float) (time_offset.x % 1000) / 1500.0f
+        : (float) (1000 - (time_offset.x % 1000)) / 1500.0f
+      ) +
+      brightness_minimum
+    );
+    data_object->color.y = (
+      ((time_offset.y / 1000) % 2 == 0
+        ? (float) (time_offset.y % 1000) / 1500.0f
+        : (float) (1000 - (time_offset.y % 1000)) / 1500.0f
+      ) +
+      brightness_minimum
+    );
+    data_object->color.z = (
+      ((time_offset.z / 1000) % 2 == 0
+        ? (float) (time_offset.z % 1000) / 1500.0f
+        : (float) (1000 - (time_offset.z % 1000)) / 1500.0f
+      ) +
+      brightness_minimum
+    );
   }
-
-  a[0].x = -sinf(time / 4000.0f) * 0.9 - 0.1;
-  a[0].y = -sinf(time / 4000.0f) * 0.9 - 0.1;
-
-  a[1].x = sinf(time / 4000.0f) * 0.9 + 0.1;
-  a[1].y = -sinf(time / 4000.0f) * 0.9 - 0.1;
-
-  a[2].x = sinf(time / 4000.0f) * 0.9 + 0.1;
-  a[2].y = sinf(time / 4000.0f) * 0.9 + 0.1;
-
-  a[3].x = -sinf(time / 4000.0f) * 0.9 - 0.1;
-  a[3].y = sinf(time / 4000.0f) * 0.9 + 0.1;
-
-  metil_kit_data_frame_object* data_object = scene->objects[
-    0
-  ]->data.contents;
-
-  data_object->color.x = fabs(sinf(((float) (scene->time % 2000) / 2000.0f) * M_PI / 2.0f));
-  data_object->color.y = fabs(sinf(((float) (scene->time % 3000) / 3000.0f) * M_PI / 2.0f));
-  data_object->color.z = fabs(sinf(((float) (scene->time % 1000) / 1000.0f) * M_PI / 2.0f));
 }
 
 void example_2d_scene_destroy(
   struct metil_scene* scene
 ) {
-  [scene->objects[0]->data release];
-  [scene->objects[0]->indices release];
   [scene->objects[0]->vertices release];
+  [scene->objects[0]->indices release];
 
-  scene->data = (void*)0;
+  for (
+    unsigned char index_object = 0;
+    index_object < scene->length_objects;
+    ++index_object
+  ) {
+    [scene->objects[index_object]->data release];
+  }
 
   metil_scene_destroy_default(scene);
 }
