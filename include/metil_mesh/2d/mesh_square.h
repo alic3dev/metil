56fc13ea0421f855ff358e87e38a94ae369abfe3
alic3 <alic3dev@gmail.com>
2025-09-25 19:54:51 -0700

    `metil_mesh/2d/mesh_square`:add

---
+++ include/metil_mesh/2d/mesh_square.h
@@ -0,0 +1,11 @@
+#ifndef __metil_mesh_2d_mesh_square_h
+#define __metil_mesh_2d_mesh_square_h
+
+#include <metil_mesh/mesh.h>
+
+void metil_mesh_square_initialize(
+  struct metil_mesh*,
+  float
+);
+
+#endif
---
+++ sources/metil_mesh/2d/mesh_square.c
@@ -0,0 +1,63 @@
+#include <metil_mesh/2d/mesh_square.h>
+
+#include <metil_mesh/mesh.h>
+
+#include <stdlib.h>
+
+void metil_mesh_square_initialize(
+  struct metil_mesh* metil_mesh,
+  float size
+) {
+  metil_mesh_initialize(
+    metil_mesh
+  );
+
+  metil_mesh->size.x = size;
+  metil_mesh->size.y = size;
+  metil_mesh->size.z = 0.0f;
+
+  float size_half = size / 2.0f;
+
+  metil_mesh->length_vertices = 4;
+  metil_mesh->length_indices = 6;
+
+  metil_mesh->indices = realloc(
+    metil_mesh->indices,
+    sizeof(unsigned int) *
+    metil_mesh->length_indices
+  );
+
+  metil_mesh->vertices = realloc(
+    metil_mesh->vertices,
+    sizeof(struct clic3_vector4_float) *
+    metil_mesh->length_vertices
+  );
+
+  metil_mesh->vertices[0].x = -size_half;
+  metil_mesh->vertices[0].y = -size_half;
+  metil_mesh->vertices[0].z = 0.0f;
+  metil_mesh->vertices[0].w = 1.0f;
+
+  metil_mesh->vertices[1].x = size_half;
+  metil_mesh->vertices[1].y = -size_half;
+  metil_mesh->vertices[1].z = 0.0f;
+  metil_mesh->vertices[1].w = 1.0f;
+
+  metil_mesh->vertices[2].x = -size_half;
+  metil_mesh->vertices[2].y = size_half;
+  metil_mesh->vertices[2].z = 0.0f;
+  metil_mesh->vertices[2].w = 1.0f;
+
+  metil_mesh->vertices[3].x = size_half;
+  metil_mesh->vertices[3].y = size_half;
+  metil_mesh->vertices[3].z = 0.0f;
+  metil_mesh->vertices[3].w = 1.0f;
+
+  metil_mesh->indices[0] = 0;
+  metil_mesh->indices[1] = 1;
+  metil_mesh->indices[2] = 2;
+
+  metil_mesh->indices[3] = 2;
+  metil_mesh->indices[4] = 3;
+  metil_mesh->indices[5] = 1;
+}
