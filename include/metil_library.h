c5bace680dd73ba108041ad54119e14e5ab043ba
alic3 <alic3dev@gmail.com>
2025-09-30 09:26:37 -0700

    `metil_library`:add_fps_display

--- include/metil_library.h
+++ include/metil_library.h
@@ -5,8 +5,14 @@
 
 struct metil_library {
   id<MTLLibrary> library;
+
   id<MTLFunction> function_fragment;
   id<MTLFunction> function_vertex;
+
+  id<MTLLibrary> library_fps_display;
+  
+  id<MTLFunction> function_fragment_fps_display;
+  id<MTLFunction> function_vertex_fps_display;
 };
 
 extern struct metil_library metil_library;
--- sources/metil_library.m
+++ sources/metil_library.m
@@ -1,7 +1,13 @@
 #include <metil_library.h>
 
+#include <Metal/MTLDevice.h>
+
 struct metil_library metil_library = {
   .library = (void*)0,
   .function_fragment = (void*)0,
-  .function_vertex = (void*)0
+  .function_vertex = (void*)0,
+
+  .library_fps_display = (void*)0,
+  .function_fragment_fps_display = (void*)0,
+  .function_vertex_fps_display = (void*)0
 };
