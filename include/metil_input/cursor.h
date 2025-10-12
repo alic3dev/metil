a268aca5a97c0c44aad780bc17802181364f4ce9
alic3 <alic3dev@gmail.com>
2025-10-11 18:54:53 -0700

    add cursor dragging support and add delta values during drag events

--- include/metil_input/cursor.h
+++ include/metil_input/cursor.h
@@ -4,6 +4,8 @@
 #include <clic3_vector.h>
 
 extern unsigned char metil_input_locked_cursor;
+extern unsigned char metil_input_dragging_cursor;
+
 extern struct clic3_vector2_float metil_input_delta_cursor;
 extern struct clic3_vector2_float metil_input_position_cursor_screen;
 extern struct clic3_vector2_float metil_input_position_cursor_window;
--- sources/metil_input/cursor.c
+++ sources/metil_input/cursor.c
@@ -1,6 +1,7 @@
 #include <metil_input/cursor.h>
 
 unsigned char metil_input_locked_cursor = 0;
+unsigned char metil_input_dragging_cursor = 0;
 
 struct clic3_vector2_float metil_input_delta_cursor = {
   .x = 0.0f,
