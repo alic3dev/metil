fcf992bf1136d215d6b3efd0b32d2433540154cb
alic3 <alic3dev@gmail.com>
2025-11-03 18:07:07 -0800

    `metil_renderer_data_object`:add->{`size`};

--- include/metil_rendering/metil_renderer_data_object.h
+++ include/metil_rendering/metil_renderer_data_object.h
@@ -10,13 +10,9 @@ struct metil_renderer_data_object {
 
   matrix_float4x4 view_model_matrix_projection;
   struct clic3_vector3_float position;
-
-  float width;
-  float height;
-  float depth;
+  struct clic3_vector3_float size;
 
   struct clic3_vector4_float color;
-  int mode_texture;
 
   unsigned int noise;
 };
