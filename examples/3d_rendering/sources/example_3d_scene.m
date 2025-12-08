df879a8ec3321ee2ebc42a2ec3c5f0ed234da812
alic3 <alic3dev@gmail.com>
2025-12-08 03:49:31 -0800

    `example_3d_scene`:remove->{`id`};

--- examples/3d_rendering/sources/example_3d_scene.m
+++ examples/3d_rendering/sources/example_3d_scene.m
@@ -82,7 +82,6 @@ void example_3d_scene_initialize(
       object->data.contents
     );
 
-    data_object->id = 0;
     data_object->color.x = (
       (float) (index_renderable % 10) / 10.0f
     );
