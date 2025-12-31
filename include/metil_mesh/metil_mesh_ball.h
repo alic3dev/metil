1e64816dd0f2e57557b5131a81f6ea8de0d6e560
alic3 <alic3dev@gmail.com>
2025-12-31 06:50:24 -0800

    `metil_mesh_ball`:add

---
+++ include/metil_mesh/metil_mesh_ball.h
@@ -0,0 +1,14 @@
+#ifndef __metil_mesh_metil_mesh_ball_h
+#define __metil_mesh_metil_mesh_ball_h
+
+#include <metil_mesh/metil_mesh.h>
+
+#include <clic3_vector.h>
+
+void metil_mesh_ball_initialize(
+  struct metil_mesh*,
+  float,
+  struct clic3_vector2_unsigned_short_int
+);
+
+#endif
