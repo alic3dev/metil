f250231b5ed58ae418184be851b8f8ca31f1ac33
alic3 <alic3dev@gmail.com>
2025-12-31 22:42:35 -0800

    swap_global_to_define->{`metil_configuration_default_audio_volume`}

--- include/metil_configuration/metil_configuration_audio.h
+++ include/metil_configuration/metil_configuration_audio.h
@@ -1,7 +1,7 @@
 #ifndef __metil_configuration_metil_configuration_audio_h
 #define __metil_configuration_metil_configuration_audio_h
 
-extern const float metil_configuration_default_audio_volume;
+#define metil_configuration_default_audio_volume 1.0f
 
 struct metil_configuration_audio {
   float volume;
