c0ee791329395a7609df4be1221656e6753f7953
alic3 <alic3dev@gmail.com>
2026-01-08 11:33:04 -0800

    collision use math_c

--- include/metil_collision/metil_collision_uncollide/metil_collision_uncollide_circular.h
+++ include/metil_collision/metil_collision_uncollide/metil_collision_uncollide_circular.h
@@ -1,18 +1,18 @@
 #ifndef __metil_collision_metil_collision_uncollide_metil_collision_uncollide_circular_h
 #define __metil_collision_metil_collision_uncollide_metil_collision_uncollide_circular_h
 
-#include <clic3_vector.h>
+#include <math_c_vector.h>
 
 void metil_collision_uncollide_circular_xz(
-  struct clic3_vector3_float*,
-  struct clic3_vector3_float*,
-  struct clic3_vector3_float*,
-  struct clic3_vector3_float*
+  struct math_c_vector3_float*,
+  struct math_c_vector3_float*,
+  struct math_c_vector3_float*,
+  struct math_c_vector3_float*
 );
 
 void metil_collision_uncollide_circular_distance_xz(
-  struct clic3_vector3_float*,
-  struct clic3_vector3_float*,
+  struct math_c_vector3_float*,
+  struct math_c_vector3_float*,
   float
 );
 
