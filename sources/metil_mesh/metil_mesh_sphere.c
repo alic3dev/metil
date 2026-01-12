786da63a57bbd86b07ecabdf45ab3033d9623de8
alic3 <alic3dev@gmail.com>
2026-01-11 17:38:07 -0800

    `metil_mesh_sphere`:add

---
+++ sources/metil_mesh/metil_mesh_sphere.c
@@ -0,0 +1,17 @@
+#include <metil_mesh/metil_mesh_ball.h>
+
+#include <metil_mesh/metil_mesh.h>
+
+#include <math_c_vector.h>
+
+void metil_mesh_sphere_initialize(
+  struct metil_mesh* metil_mesh,
+  float size,
+  struct math_c_vector2_unsigned_short_int segments
+) {
+  metil_mesh_ball_initialize(
+    metil_mesh,
+    size,
+    segments
+  );
+}
