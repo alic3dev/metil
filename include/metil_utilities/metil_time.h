15a75756b537b08b11cce23ad942ca4e9764ead2
alic3 <alic3dev@gmail.com>
2026-01-30 07:35:15 -0800

    `metil_time`:fix_microsecond_conversions

--- include/metil_utilities/metil_time.h
+++ include/metil_utilities/metil_time.h
@@ -2,14 +2,14 @@
 #define __metil_utilities_metil_time_h
 
 #define metil_time_microseconds_to_milliseconds(x) ((x) / 1000)
-#define metil_time_microseconds_to_seconds(x) ((x) / 1000000000)
-#define metil_time_microseconds_to_minutes(x) ((x) / 60000000000)
+#define metil_time_microseconds_to_seconds(x) ((x) / 1000000)
+#define metil_time_microseconds_to_minutes(x) ((x) / 60000000)
 
 #define metil_time_milliseconds_to_microseconds(x) ((x) * 1000)
 #define metil_time_milliseconds_to_seconds(x) ((x) / 1000)
 #define metil_time_milliseconds_to_minutes(x) ((x) / 60000)
 
-#define metil_time_seconds_to_microseconds(x) ((x) * 1000000000)
+#define metil_time_seconds_to_microseconds(x) ((x) * 1000000)
 #define metil_time_seconds_to_milliseconds(x) ((x) * 1000)
 #define metil_time_seconds_to_minutes(x) ((x) / 60)
 
