ea17d82885bd141838061820b6235323205a6738
alic3 <alic3dev@gmail.com>
2026-01-30 07:27:19 -0800

    `metil_time`:add_additional_defines

--- include/metil_utilities/metil_time.h
+++ include/metil_utilities/metil_time.h
@@ -1,9 +1,17 @@
 #ifndef __metil_utilities_metil_time_h
 #define __metil_utilities_metil_time_h
 
+#define metil_time_microseconds_to_milliseconds(x) ((x) / 1000)
+#define metil_time_microseconds_to_seconds(x) ((x) / 1000000000)
+#define metil_time_microseconds_to_minutes(x) ((x) / 60000000000)
+
+#define metil_time_milliseconds_to_microseconds(x) ((x) * 1000)
+#define metil_time_milliseconds_to_seconds(x) ((x) / 1000)
+#define metil_time_milliseconds_to_minutes(x) ((x) / 60000)
+
 #define metil_time_seconds_to_microseconds(x) ((x) * 1000000000)
 #define metil_time_seconds_to_milliseconds(x) ((x) * 1000)
-#define metil_time_microseconds_to_milliseconds(x) ((x) / 1000)
+#define metil_time_seconds_to_minutes(x) ((x) / 60)
 
 unsigned long int metil_time_milliseconds_get();
 
