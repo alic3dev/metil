5d7db0c35e16a7ccfe6faa44bc0efc113b6dc9b1
alic3 <alic3dev@gmail.com>
2026-02-04 00:53:06 -0800

    `examples/2d_rendering`:update

--- examples/2d_rendering/sources/example_2d_scene.m
+++ examples/2d_rendering/sources/example_2d_scene.m
@@ -1,7 +1,11 @@
 #include <example_2d_scene.h>
 
 #include <metil.h>
+#include <metil_group.h>
+#include <metil_mesh/metil_mesh_2d/metil_mesh_circle.h>
+#include <metil_mesh/metil_mesh_2d/metil_mesh_rectangle.h>
 #include <metil_mesh/metil_mesh_2d/metil_mesh_square.h>
+#include <metil_mesh/metil_mesh_2d/metil_mesh_triangle.h>
 #include <metil_object.h>
 #include <metil_player/metil_player.h>
 #include <metil_positioning.h>
@@ -10,191 +14,321 @@
 #include <metil_rendering/metil_renderer_data_object.h>
 #include <metil_scenes/metil_scene.h>
 
-#include <math_c_vector.h>
 #include <math_c_pi.h>
 #include <math_c_sine.h>
+#include <math_c_vector.h>
 
 #include <clic3_memory.h>
 
 void example_2d_scene_initialize(
   struct metil* metil,
-  struct metil_scene* scene
+  struct metil_scene* metil_scene
 ) {
   metil_scene_initialize_with_renderables(
     metil,
-    scene,
-    2
+    metil_scene,
+    5
   );
 
-  scene->player.poll_input = metil_player_poll_input_null;
+  metil_scene->player.poll_input = (
+    metil_player_poll_input_null
+  );
 
-  scene->poll = example_2d_scene_poll;
-
-  #undef M_PI
-  #define M_PI math_c_pi_calculate(1000)
-
-  printf("%.100f\n", M_PI);
-  printf("%.100f\n", math_c_pi_calculate(1000));
+  metil_scene->poll = (
+    example_2d_scene_poll
+  );
 
   for (
     unsigned char index_renderable = 0;
-    index_renderable < scene->length_renderables;
+    index_renderable < metil_scene->length_renderables;
     ++index_renderable
   ) {
     metil_renderable_initialize_at_index(
-      scene->renderables,
+      metil_scene->renderables,
       index_renderable,
-      metil_renderable_type_object
+      metil_renderable_type_group
     );
 
-    struct metil_object* object = (
-      scene->renderables[
+    struct metil_group* metil_group = (
+      metil_scene->renderables[
         index_renderable
       ].renderable
     );
 
-    metil_mesh_initialize(
-      &object->mesh
+    metil_group_add_length_initialize(
+      metil_group,
+      100,
+      metil_renderable_type_object
     );
-
-    object->mesh.length_vertices = 1000;
-    object->mesh.length_indices = 1000;
-    object->type_primitive = MTLPrimitiveTypeLineStrip;
-
-    clic3_memory_reallocate_raw(
-      &object->mesh.vertices,
-      sizeof(struct math_c_vector4_float) * object->mesh.length_vertices
-    );
-
-    clic3_memory_reallocate_raw(
-      &object->mesh.indices,
-      sizeof(unsigned int) * object->mesh.length_indices
-    );
-
-    float mult = M_PI * 4.20;
 
     for (
-      unsigned int i = 0; i < object->mesh.length_vertices; ++i
+      unsigned int index_group_renderable = 0;
+      index_group_renderable < metil_group->length;
+      ++index_group_renderable
     ) {
-      float ssin;
+      struct metil_object* metil_object = (
+        metil_group->renderables[
+          index_group_renderable
+        ]->renderable
+      );
 
-      ssin = ((float) i / (float) (object->mesh.length_vertices - 1)) * mult - 0.5f;
+      float percentage_group = (
+        (float)
+        index_group_renderable /
+        (float)
+        (
+          metil_group->length -
+          1
+        )
+      );
 
-      if (index_renderable == 0) {
-        ssin = math_c_tangent(
-          ssin,
-          M_PI
+      if (
+        index_renderable > 0
+      ) {
+        metil_mesh_circle_initialize(
+          &metil_object->mesh,
+          (
+            0.001f *
+            (
+              index_group_renderable +
+              1
+            )
+          ),
+          (
+            index_group_renderable +
+            2
+          )
         );
       } else {
-        
-        ssin = tan(ssin);
+        metil_mesh_square_initialize(
+          &metil_object->mesh,
+          (
+            1.6f *
+            (
+              1.0f -
+              percentage_group
+            )
+          )
+        );
       }
 
-      object->mesh.vertices[i].x = (-1.0f) + (((float) i / (float) (object->mesh.length_vertices - 1)) * 2.0f);
-      object->mesh.vertices[i].y = ssin;
-      object->mesh.vertices[i].z = 0.0f;
-      object->mesh.vertices[i].w = 1.0f;
+      metil_object->positioning = (
+        metil_positioning_static
+      );
 
-      object->mesh.indices[i] = i;
+      metil_object_buffers_initialize(
+        metil_object,
+        metil->renderer_interface.metal_device
+      );
+
+      struct metil_renderer_data_object* metil_renderer_data_object = (
+        metil_object->buffers_vertex[
+          metil_object_buffer_default_index_data
+        ].buffer.contents
+      );
+
+      if (
+        index_renderable > 0
+      ) {
+        metil_renderer_data_object->colour.x = (
+          percentage_group
+        );
+
+        metil_renderer_data_object->colour.y = (
+          percentage_group *
+          0.75f +
+          0.25f
+        );
+
+        metil_renderer_data_object->colour.w = (
+          1.0f
+        );
+      } else {
+        metil_renderer_data_object->colour.x = (
+          1.0f
+        );
+
+        metil_renderer_data_object->colour.y = (
+          1.0f -
+          (
+            percentage_group
+          )
+        );
+
+        metil_renderer_data_object->colour.z = (
+          metil_renderer_data_object->colour.y
+        );
+
+        metil_renderer_data_object->colour.w = (
+          1.0f -
+          (
+            percentage_group
+          )
+        );
+      }
     }
-
-    object->positioning = (
-      metil_positioning_absolute
-    );
-
-    metil_object_buffers_initialize(
-      object,
-      metil->renderer_interface.metal_device
-    );
-
-    struct metil_renderer_data_object* data_object = (
-      object->buffers_vertex[
-        metil_object_buffer_default_index_data
-      ].buffer.contents
-    );
-
-    if (index_renderable != 0) {
-      data_object->colour.x = (
-        0.0f
-      );
-
-      data_object->colour.y = (
-        1.0f
-      );
-
-      data_object->colour.z = (
-        0.0f
-      );
-    }
-
-    data_object->colour.w = 1.0f;
-
-    // object->position.x = (
-    //   (float) (index_renderable % 10)
-    // ) * 0.2f - 0.9f;
-
-    // object->position.y = (
-    //   (float) (index_renderable / 10)
-    // ) * 0.2f - 0.9f;
-
-    // object->position.z = (
-    //   1.0f
-    // );
   }
 }
 
 void example_2d_scene_poll(
   struct metil* metil,
-  struct metil_scene* scene
+  struct metil_scene* metil_scene
 ) {
   metil_scene_poll_default(
     metil,
-    scene
+    metil_scene
   );
-
-  return;
-
-  float brightness_minimum = 0.25f;
 
   for (
     unsigned char index_renderable = 0;
-    index_renderable < scene->length_renderables;
+    index_renderable < metil_scene->length_renderables;
     ++index_renderable
   ) {
-    struct metil_renderer_data_object* data_object = (
-      (struct metil_object*) scene->renderables[
+    struct metil_group* metil_group = (
+      metil_scene->renderables[
         index_renderable
       ].renderable
-    )->buffers_vertex[
-      metil_object_buffer_default_index_data
-    ].buffer.contents;
+    );
 
-    struct math_c_vector3_unsigned_int time_offset = {
-      .x = scene->time + (index_renderable + 2) * 20,
-      .y = scene->time + (index_renderable + 3) * 30,
-      .z = scene->time + (index_renderable + 1) * 10
-    };
+    for (
+      unsigned int index_group_renderable = 0;
+      index_group_renderable < metil_group->length;
+      ++index_group_renderable
+    ) {
+      struct metil_object* metil_object = (
+        metil_group->renderables[
+          index_group_renderable
+        ]->renderable
+      );
 
-    data_object->colour.x = (
-      ((time_offset.x / 1000) % 2 == 0
-        ? (float) (time_offset.x % 1000) / 1500.0f
-        : (float) (1000 - (time_offset.x % 1000)) / 1500.0f
-      ) +
-      brightness_minimum
-    );
-    data_object->colour.y = (
-      ((time_offset.y / 1000) % 2 == 0
-        ? (float) (time_offset.y % 1000) / 1500.0f
-        : (float) (1000 - (time_offset.y % 1000)) / 1500.0f
-      ) +
-      brightness_minimum
-    );
-    data_object->colour.z = (
-      ((time_offset.z / 1000) % 2 == 0
-        ? (float) (time_offset.z % 1000) / 1500.0f
-        : (float) (1000 - (time_offset.z % 1000)) / 1500.0f
-      ) +
-      brightness_minimum
-    );
+      float percentage_group = (
+        (float)
+        index_group_renderable /
+        (float)
+        (
+          metil_group->length -
+          1
+        )
+      );
+
+      struct metil_renderer_data_object* metil_renderer_data_object = (
+        metil_object->buffers_vertex[
+          metil_object_buffer_default_index_data
+        ].buffer.contents
+      );
+
+      if (
+        index_renderable > 0
+      ) {
+        metil_object->position.x = (
+          math_c_sine(
+            (
+              (float)
+              metil->renderer_interface.rendering_properties->frame /
+              60.0f +
+              index_renderable
+            ),
+            math_c_pi
+          ) *
+          (
+            percentage_group
+          ) *
+          (
+            index_group_renderable % 2 == 0
+            ? -1.0f
+            : 1.0f
+          )
+        );
+
+        metil_object->position.y = (
+          math_c_cosine(
+            (
+              (float)
+              metil->renderer_interface.rendering_properties->frame /
+              (
+                60.0f /
+                (
+                  (float)
+                  (
+                    index_renderable +
+                    1
+                  ) /
+                  4.0f
+                )
+              ) +
+              index_renderable
+            ),
+            math_c_pi
+          ) *
+          (
+            percentage_group
+          ) *
+          (
+            index_group_renderable % 2 == 0
+            ? -1.0f
+            : 1.0f
+          )
+        );
+
+        float tangent = (
+          math_c_tangent(
+            (
+              (
+                (float)
+                metil->renderer_interface.rendering_properties->frame /
+                (
+                  60.0f
+                )
+              ) *
+              math_c_pi *
+              1.0f +
+              (
+                index_renderable *
+                metil_group->length
+              ) +
+              index_group_renderable
+            ),
+            math_c_pi
+          ) /
+          math_c_pi
+        );
+
+        if (
+          tangent < 0.0f
+        ) {
+          tangent = (
+            -tangent
+          );
+        }
+        
+        if (
+          tangent > 1.0f
+        ) {
+          tangent = 1.0f;
+        }
+
+        metil_renderer_data_object->colour.y = (
+          percentage_group *
+          0.75f +
+          (
+            0.25f -
+            (
+              0.25f *
+              tangent
+            )
+          )
+        );
+
+        metil_renderer_data_object->colour.x = (
+          metil_renderer_data_object->colour.y  
+        );
+      } else {
+        metil_object->rotation.z = (
+          metil_object->rotation.z +
+          0.0001f *
+          index_group_renderable
+        );
+      }
+    }
   }
 }
