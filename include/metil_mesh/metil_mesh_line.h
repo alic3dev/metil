3f7bcd4bbe673379f80cd850cbcd25d0d6d0e498
alic3 <alic3dev@gmail.com>
2026-02-03 18:50:01 -0800

    `metil_mesh_line`:add

---
+++ include/metil_mesh/metil_mesh_line.h
@@ -0,0 +1,14 @@
+#ifndef __metil_metil_mesh_metil_mesh_line_h
+#define __metil_metil_mesh_metil_mesh_line_h
+
+#include <metil_mesh/metil_mesh.h>
+
+#include <math_c_vector.h>
+
+void metil_mesh_line_initialize(
+  struct metil_mesh*,
+  unsigned int,
+  struct math_c_vector3_float*
+);
+
+#endif
