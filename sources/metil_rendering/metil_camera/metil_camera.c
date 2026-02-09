4e6b7ac67aef25ace996e920ab17971ad50d37c7
alic3 <alic3dev@gmail.com>
2026-02-08 20:28:17 -0800

    `metil_camera`:set_field_of_view_to_145_degrees

--- sources/metil_rendering/metil_camera/metil_camera.c
+++ sources/metil_rendering/metil_camera/metil_camera.c
@@ -11,8 +11,8 @@ void metil_camera_initialize(
     metil_camera_mode_first_person
   );
 
-  metil_camera->field_of_view.x = 0.0f;
-  metil_camera->field_of_view.y = 0.0f;
+  metil_camera->field_of_view.x = 1.45f;
+  metil_camera->field_of_view.y = 1.45f;
 
   if (
     metil_camera->initialized == 0
@@ -48,12 +48,8 @@ void metil_camera_ratio_aspect_set(
     metil_camera_size_view->y
   );
 
-  metil_camera_field_of_view_set(
-    metil_camera,
-    (struct math_c_vector2_float) {
-      .x = 0.9f,
-      .y = 0.9f
-    }
+  metil_camera_normalization_set(
+    metil_camera
   );
 }
 
