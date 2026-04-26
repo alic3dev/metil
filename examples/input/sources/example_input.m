35bf4390cc34d6c771067aaad1e0bc7f939670ec
alic3 <alic3dev@gmail.com>
2026-04-25 21:25:26 -0700

    `example_input_pipeline_index`:convert_to_structure

--- examples/input/sources/example_input.m
+++ examples/input/sources/example_input.m
@@ -3,6 +3,8 @@
 #include <example_input_pipeline_index.h>
 #include <example_input_scene.h>
 
+#include <clic3_memory.h>
+
 #include <metil.h>
 #include <metil_initialize.h>
 #include <metil_library.h>
@@ -24,22 +26,98 @@ void example_input_renderer_on_initialize(
   struct metil* metil,
   void* data
 ) {
+  metil->data = (
+    clic3_memory_allocate_raw(
+      sizeof(
+        struct example_input_pipeline_index
+      )
+    )
+  );
+
+  struct example_input_pipeline_index* example_input_pipeline_index = (
+    metil->data
+  );
+
+  example_input_pipeline_index_initialize(
+    example_input_pipeline_index
+  );
+
   metil_library_initialize(
     &metil->library,
     metil->renderer_interface.metal_device,
-    @"model_fragment",
-    @"model_vertex"
+    @"example_input_ground_fragment",
+    @"example_input_ground_vertex"
   );
 
-  example_input_pipeline_index_model_item = [
+  example_input_pipeline_index->model_player_body = [
     metil->renderer_interface.renderer
     pipeline_add: [
       metil->library.library
-      newFunctionWithName: @"model_item_fragment"
+      newFunctionWithName: @"example_input_model_player_body_fragment"
     ]
     function_vertex: [
       metil->library.library
-      newFunctionWithName: @"model_item_vertex"
+      newFunctionWithName: @"example_input_model_player_body_vertex"
+    ]
+  ];
+
+  example_input_pipeline_index->model_player_shirt = [
+    metil->renderer_interface.renderer
+    pipeline_add: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_player_shirt_fragment"
+    ]
+    function_vertex: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_player_shirt_vertex"
+    ]
+  ];
+
+  example_input_pipeline_index->model_player_pants = [
+    metil->renderer_interface.renderer
+    pipeline_add: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_player_pants_fragment"
+    ]
+    function_vertex: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_player_pants_vertex"
+    ]
+  ];
+
+  example_input_pipeline_index->model_skateboard_deck = [
+    metil->renderer_interface.renderer
+    pipeline_add: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_deck_fragment"
+    ]
+    function_vertex: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_deck_vertex"
+    ]
+  ];
+
+  example_input_pipeline_index->model_skateboard_truck = [
+    metil->renderer_interface.renderer
+    pipeline_add: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_truck_fragment"
+    ]
+    function_vertex: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_truck_vertex"
+    ]
+  ];
+
+  example_input_pipeline_index->model_skateboard_wheel = [
+    metil->renderer_interface.renderer
+    pipeline_add: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_wheel_fragment"
+    ]
+    function_vertex: [
+      metil->library.library
+      newFunctionWithName: @"example_input_model_skateboard_wheel_vertex"
     ]
   ];
 
--- examples/input/sources/example_input_pipeline_index.c
+++ examples/input/sources/example_input_pipeline_index.c
@@ -1,5 +1,29 @@
 #include <example_input_pipeline_index.h>
 
-unsigned short int example_input_pipeline_index_model_item = (
-  0x00
-);
+void example_input_pipeline_index_initialize(
+  struct example_input_pipeline_index* example_input_pipeline_index
+) {
+  example_input_pipeline_index->model_player_body = (
+    0x00
+  );
+
+  example_input_pipeline_index->model_player_pants = (
+    0x00
+  );
+
+  example_input_pipeline_index->model_player_shirt = (
+    0x00
+  );
+
+  example_input_pipeline_index->model_skateboard_deck = (
+    0x00
+  );
+
+  example_input_pipeline_index->model_skateboard_truck = (
+    0x00
+  );
+
+  example_input_pipeline_index->model_skateboard_wheel = (
+    0x00
+  );
+}
