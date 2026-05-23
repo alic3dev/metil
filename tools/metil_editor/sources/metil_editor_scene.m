abbf5b7c3dc985677afc6d85f2130f2f0b8ac66f
alic3dev <alice@alic3.dev>
2026-05-22 20:15:08 -0700

    `metil_editor_scene`:standardize_white_space

--- tools/metil_editor/sources/metil_editor_scene.m
+++ tools/metil_editor/sources/metil_editor_scene.m
@@ -25,7 +25,7 @@ void metil_editor_scene_initialize(
     metil_scene,
     metil_editor_scene_length_renderables
   );
-
+  
   metil_scene->data = (
     clic3_memory_allocate_raw(
       sizeof(
@@ -33,15 +33,15 @@ void metil_editor_scene_initialize(
       )
     )
   );
-
+  
   metil_scene->poll = (
     metil_editor_scene_poll
   );
-
+  
   metil_scene->destroy = (
     metil_editor_scene_destroy
   );
-
+  
   for (
     unsigned char index_renderable = (
       0x00
@@ -61,7 +61,7 @@ void metil_editor_scene_initialize(
           index_renderable,
           metil_renderable_type_group
         );
-
+        
         break;
       }
       default: {
@@ -70,36 +70,36 @@ void metil_editor_scene_initialize(
           index_renderable,
           metil_renderable_type_object
         );
-
+        
         break;
       }
     }
   }
-
+  
   struct metil_group* metil_group_grids = (
     metil_scene->renderables[
       metil_editor_scene_index_renderable_group_grids
     ].renderable
   );
-
+  
   struct metil_object* metil_object_lines = (
     metil_scene->renderables[
       metil_editor_scene_index_renderable_lines
     ].renderable
   );
-
+  
   struct metil_object* metil_object_points = (
     metil_scene->renderables[
       metil_editor_scene_index_renderable_points
     ].renderable
   );
-
+  
   metil_group_add_length_initialize(
     metil_group_grids,
     0x06,
     metil_renderable_type_object
   );
-
+  
   for (
     unsigned char index_grid = (
       0x00
@@ -115,12 +115,12 @@ void metil_editor_scene_initialize(
         index_grid
       ]->renderable
     );
-
+    
     unsigned char subgrid = (
       index_grid %
       0x02
     );
-
+    
     unsigned long int length_cells = (
       (
         subgrid ==
@@ -129,7 +129,7 @@ void metil_editor_scene_initialize(
       ? 0x64
       : 0x03e8
     );
-
+      
     metil_mesh_celled_grid_initialize(
       &metil_object_grid->mesh,
       (struct math_c_vector2_float) {
@@ -149,18 +149,18 @@ void metil_editor_scene_initialize(
         )
       }
     );
-
+    
     metil_object_buffers_initialize(
       metil_object_grid,
       metil->renderer_interface.metal_device
     );
-
+    
     struct metil_renderer_data_object* metil_renderer_data_object = (
       metil_object_grid->buffers_vertex[
         metil_object_buffer_default_index_data
       ].buffer.contents
     );
-
+      
     metil_renderer_data_object->colour.w = (
       (
         subgrid ==
@@ -169,7 +169,7 @@ void metil_editor_scene_initialize(
       ? 0.25f
       : 0.025f
     );
-
+    
     switch (
       index_grid /
       0x02
@@ -179,55 +179,55 @@ void metil_editor_scene_initialize(
           metil_object_grid->mesh.size.y /
           0x64
         );
-
+      
         metil_object_grid->rotation.x = (
           math_c_pi_half
         );
-
+        
         break;
       }
       case 0x01: {
         metil_object_grid->rotation.y = (
           math_c_pi_half
         );
-
+        
         break;
       }
       default: {
         break;
       }
     }
-
+    
     metil_object_grid->type_primitive = (
       MTLPrimitiveTypeLine
     );
   }
-
+  
   metil_mesh_initialize(
     &metil_object_lines->mesh
   );
-
+  
   metil_mesh_initialize(
     &metil_object_points->mesh
   );
-
+  
   metil_object_buffers_initialize(
     metil_object_lines,
     metil->renderer_interface.metal_device
   );
-
+  
   metil_object_buffers_add(
     metil_object_points,
     metil->renderer_interface.metal_device,
     metil_object_buffer_type_vertex
   );
-
+  
   metil_object_buffers_add(
     metil_object_points,
     metil->renderer_interface.metal_device,
     metil_object_buffer_type_vertex
   );
-
+  
   metil_object_points->buffers_vertex[
     0x00
   ].buffer = (
@@ -235,44 +235,44 @@ void metil_editor_scene_initialize(
       0x00
     ].buffer
   );
-
+  
   metil_object_points->buffers_vertex[
     0x01
   ].buffer = (
     metil_object_lines->buffers_vertex[
       0x01
     ].buffer
-  );
+  );  
 
   metil_object_indices_initialize(
     metil_object_points,
     metil->renderer_interface.metal_device
   );
-
+  
   metil_object_lines->visible = (
     0x00
   );
-
+  
   metil_object_points->visible = (
     0x00
   );
-
+  
   metil_scene->player.position.x = (
     0x0a
   );
-
+  
   metil_scene->player.position.y = (
     0x0a
   );
-
+  
   metil_scene->player.position.z = -(
     0x0a
   );
-
+  
   metil_scene->player.poll_input = (
-    metil_player_poll_input_free_flying_locked
+    metil_player_poll_input_free_flying_unlocked
   );
-
+  
   metil_scene->player.rotation.y = (
     math_c_pi_half /
     0x02
@@ -287,11 +287,11 @@ void metil_editor_scene_poll(
     metil,
     metil_scene
   );
-
+  
   struct metil_editor_scene_data* metil_editor_scene_data = (
     metil_scene->data
   );
-
+  
   if (
     metil_editor_scene_data->movement_free ==
     0x01
@@ -304,31 +304,31 @@ void metil_editor_scene_poll(
       metil_scene->player.position
     );
   }
-
+  
   struct metil_object* metil_object_lines = (
     metil_scene->renderables[
       metil_editor_scene_index_renderable_lines
     ].renderable
   );
-
+  
   struct metil_object* metil_object_points = (
     metil_scene->renderables[
       metil_editor_scene_index_renderable_points
     ].renderable
   );
-
+  
   if (
     metil_object_lines->mesh.length_vertices ==
     0x00
   ) {
     metil_object_lines->visible = (
       0x00
-    );
+    ); 
   } else {    metil_object_lines->visible = (
       0x01
     );
   }
-
+  
   metil_object_points->visible = (
     metil_object_lines->visible
   );}
@@ -342,13 +342,13 @@ void metil_editor_scene_destroy(
       metil_editor_scene_index_renderable_points
     ].renderable
   );
-
+  
   metil_object_points->buffers_vertex[
     0x00
   ].buffer = (
     0x00
   );
-
+  
   metil_object_points->buffers_vertex[
     0x01
   ].buffer = (
