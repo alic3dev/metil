0bf4057331bfa5608e02c19b35280918fd6a9d3c
alic3dev <alice@alic3.dev>
2026-05-27 21:49:28 -0700

    `metil_mesh_export`:implement

--- include/metil_mesh/metil_mesh_export.h
+++ include/metil_mesh/metil_mesh_export.h
@@ -1,4 +1,23 @@
-#ifndef __metil_mesh_metil_mesh_parse_h
-#define __metil_mesh_metil_mesh_parse_h
+#ifndef __metil_metil_mesh_metil_mesh_export_h
+#define __metil_metil_mesh_metil_mesh_export_h
+
+#include <metil_mesh/metil_mesh.h>
+#include <metil_status.h>
+
+#include <math_c_vector.h>
+
+metil_status metil_mesh_export(
+  struct metil_mesh*,
+  char* path_file_export
+);
+
+metil_status metil_mesh_export_raw(
+  unsigned int,
+  unsigned int,
+  unsigned int*,
+  struct math_c_vector4_float*,
+  struct math_c_vector3_float*,
+  char*
+);
 
 #endif
