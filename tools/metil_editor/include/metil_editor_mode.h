6ee2475d5355ece4821a17b9d2b3b1aa11024da4
alic3dev <alice@alic3.dev>
2026-05-27 19:29:18 -0700

    `metil_editor_mode`:add

---
+++ tools/metil_editor/include/metil_editor_mode.h
@@ -0,0 +1,9 @@
+#ifndef __metil_tools_metil_editor_metil_editor_mode_h
+#define __metil_tools_metil_editor_metil_editor_mode_h
+
+enum metil_editor_mode {
+  metil_editor_mode_vertices = 0b10000000,
+  metil_editor_mode_indices  = 0b01000000
+};
+
+#endif
