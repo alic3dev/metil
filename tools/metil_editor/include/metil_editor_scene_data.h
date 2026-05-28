dc464225d2ff6e5850c97b5ecb0fb0ae3b36f749
alic3dev <alice@alic3.dev>
2026-05-27 19:29:38 -0700

    `metil_editor_scene_data`:add->{`mode`};

--- tools/metil_editor/include/metil_editor_scene_data.h
+++ tools/metil_editor/include/metil_editor_scene_data.h
@@ -1,12 +1,16 @@
 #ifndef __metil_tools_metil_editor_metil_editor_scene_data_h
 #define __metil_tools_metil_editor_metil_editor_scene_data_h
 
+#include <metil_editor_mode.h>
+
 struct metil_editor_scene_data {
   struct math_c_vector3_float position;
 
   struct math_c_vector3_float position_player;
 
   unsigned char movement_free;
+
+  unsigned char mode;
 };
 
 #endif
