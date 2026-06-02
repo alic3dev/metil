8815647561542140042ada813413a7d71158d880
alic3dev <alice@alic3.dev>
2026-06-01 23:42:17 -0700

    `metil_paths_constants`:formatting

--- include/metil_paths/metil_paths_constants.h
+++ include/metil_paths/metil_paths_constants.h
@@ -1,13 +1,13 @@
 #ifndef __metil_paths_metil_paths_constants_h
 #define __metil_paths_metil_paths_constants_h
 
-#define metil_paths_length_directory_configuration 8
+#define metil_paths_length_directory_configuration 0x08
 #if target_os_ios
-#define metil_paths_length_directory_resources 2
+#define metil_paths_length_directory_resources 0x02
 #else
-#define metil_paths_length_directory_resources 13
+#define metil_paths_length_directory_resources 0x0d
 #endif
-#define metil_paths_length_directory_textures 9
+#define metil_paths_length_directory_textures 0x09
 
 #define metil_paths_directory_configuration ".config/"
 
