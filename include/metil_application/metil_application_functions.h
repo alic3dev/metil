5dc3894e86f01c3f857fa5d7d053e557c8b2dd96
alic3dev <alice@alic3.dev>
2026-06-02 06:08:43 -0700

    `metil_application_functions`:add

---
+++ include/metil_application/metil_application_functions.h
@@ -0,0 +1,33 @@
+#ifndef __metil_metil_application_metil_application_functions_h
+#define __metil_metil_application_metil_application_functions_h
+
+#include <metil_application/metil_application_mapping.h>
+
+#include <QuartzCore/CAMetalLayer.h>
+
+unsigned char metil_application_function_display_sync_get(
+  struct metil_application_mapping* _Nonnull
+);
+
+unsigned char metil_application_function_display_sync_get_raw(
+  CAMetalLayer* _Nonnull
+);
+
+void metil_application_function_display_sync_lock(
+  struct metil_application_mapping* _Nonnull
+);
+
+void metil_application_function_display_sync_lock_raw(
+  CAMetalLayer* _Nonnull
+);
+
+void metil_application_function_display_sync_unlock(
+  struct metil_application_mapping* _Nonnull
+);
+
+void metil_application_function_display_sync_unlock_raw(
+  CAMetalLayer* _Nonnull
+);
+  
+#endif
+
---
+++ sources/metil_application/metil_application_functions.m
@@ -0,0 +1,55 @@
+#include <metil_application/metil_application_functions.h>
+
+#include <metil_application/metil_application_mapping.h>
+
+#include <QuartzCore/CAMetalLayer.h>
+
+unsigned char metil_application_function_display_sync_get(
+  struct metil_application_mapping* metil_application_mapping
+) {
+  return (
+    metil_application_function_display_sync_get_raw(
+      metil_application_mapping->layer
+    )
+  );
+}
+
+unsigned char metil_application_function_display_sync_get_raw(
+  CAMetalLayer* metal_layer
+) {
+  return (
+    metal_layer.displaySyncEnabled
+  );
+}
+
+void metil_application_function_display_sync_lock(
+  struct metil_application_mapping* metil_application_mapping
+) {
+  metil_application_function_display_sync_lock_raw(
+    metil_application_mapping->layer
+  );
+}
+
+void metil_application_function_display_sync_lock_raw(
+  CAMetalLayer* metal_layer
+) {
+  metal_layer.displaySyncEnabled = (
+    0x01
+  );
+}
+
+void metil_application_function_display_sync_unlock(
+  struct metil_application_mapping* metil_application_mapping
+) {
+  metil_application_function_display_sync_unlock_raw(
+    metil_application_mapping->layer
+  );
+}
+
+void metil_application_function_display_sync_unlock_raw(
+  CAMetalLayer* metal_layer
+) {
+  metal_layer.displaySyncEnabled = (
+    0x00
+  );
+}
