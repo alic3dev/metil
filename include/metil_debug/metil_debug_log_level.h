f3c722130fff045bb8c7617bf3061338b70b6f60
alic3dev <alice@alic3.dev>
2026-06-01 23:43:59 -0700

    `metil_debug_log_level`:add->{`metil_debug_log_level_info`};

--- include/metil_debug/metil_debug_log_level.h
+++ include/metil_debug/metil_debug_log_level.h
@@ -2,9 +2,10 @@
 #define __metil_debug_metil_debug_log_level_h
 
 enum metil_debug_log_level {
-  metil_debug_log_level_all = 1,
-  metil_debug_log_level_error = 2,
-  metil_debug_log_level_none = 0
+  metil_debug_log_level_none  = 0x00,
+  metil_debug_log_level_all   = 0x01,
+  metil_debug_log_level_info   = 0x02,
+  metil_debug_log_level_error = 0x03,
 };
 
 #endif
